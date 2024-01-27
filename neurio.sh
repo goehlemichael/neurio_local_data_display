413c66c6716fe8c8d6707d36767b4518d150684c
Mike Goehle <goehlemichael@hotmail.com>
2024-01-27 12:26:40 -0500

    Update neurio.sh
    refactor

--- neurio.sh
+++ neurio.sh
@@ -9,59 +9,62 @@ OUTPUT_TERMINAL_TYPE="${TERM_NAME:=x11}"
 echo "temp data file location $TEMP_DATA_FILE"
 echo "temp gnu plot config location $TEMP_GNUPLOT_CONFIG_FILE"
 
-if command -v curl >/dev/null 2>&1; then
-    echo "curl is installed"
-else
-    echo "curl is not installed"
-fi
+check_command() {
+    if command -v $1 >/dev/null 2>&1; then
+        echo "$1 is installed"
+    else
+        echo "$1 is not installed"
+    fi
+}
 
-if command -v jq >/dev/null 2>&1; then
-    echo "jq is installed"
-else
-    echo "jq is not installed"
-fi
+write_config() {
+    local file=$1
+    shift
+    while (( "$#" )); do
+        echo "$1" >> $file
+        shift
+    done
+}
 
-if command -v gnuplot >/dev/null 2>&1; then
-    echo "gnuplot is installed"
-else
-    echo "gnuplot is not installed"
-fi
+check_command curl
+check_command jq
+check_command gnuplot
 
-if [[ -z "${DISPLAY}" ]]; then
-  echo "DISPLAY is not set. Setting it now."
-  export DISPLAY=${DISPLAY}
-fi
+: ${DISPLAY:=:0}
 
-echo "set terminal ${OUTPUT_TERMINAL_TYPE}" >> $TEMP_GNUPLOT_CONFIG_FILE
-echo "set xdata time" >> $TEMP_GNUPLOT_CONFIG_FILE
-echo "set timefmt \"%Y-%m-%d %H:%M:%S\"" >> $TEMP_GNUPLOT_CONFIG_FILE
-echo "set format x \"%H:%M:%S\"" >> $TEMP_GNUPLOT_CONFIG_FILE
-echo "set ytics font \"Times-Roman,34\"" >> $TEMP_GNUPLOT_CONFIG_FILE
-echo "set xrange [time(0)-120:time(0)]" >> $TEMP_GNUPLOT_CONFIG_FILE
-echo "set yrange [:]" >> $TEMP_GNUPLOT_CONFIG_FILE
-echo "set lmargin at screen 0.1" >> $TEMP_GNUPLOT_CONFIG_FILE
-echo "set rmargin at screen 0.9" >> $TEMP_GNUPLOT_CONFIG_FILE
-echo "set bmargin at screen 0.1" >> $TEMP_GNUPLOT_CONFIG_FILE
-echo "set tmargin at screen 0.9" >> $TEMP_GNUPLOT_CONFIG_FILE
-echo "plot \""$TEMP_DATA_FILE"\" using 1:3 with lines" >> $TEMP_GNUPLOT_CONFIG_FILE
-echo "pause 2" >> $TEMP_GNUPLOT_CONFIG_FILE
-echo "reread" >> $TEMP_GNUPLOT_CONFIG_FILE
+write_config "$TEMP_GNUPLOT_CONFIG_FILE" \
+    "set terminal ${OUTPUT_TERMINAL_TYPE}" \
+    "set xdata time" \
+    "set timefmt \"%Y-%m-%d %H:%M:%S\"" \
+    "set format x \"%H:%M:%S\"" \
+    "set ytics font \"Times-Roman,34\"" \
+    "set xrange [time(0)-120:time(0)]" \
+    "set yrange [:]" \
+    "set lmargin at screen 0.1" \
+    "set rmargin at screen 0.9" \
+    "set bmargin at screen 0.1" \
+    "set tmargin at screen 0.9" \
+    "plot \""$TEMP_DATA_FILE"\" using 1:3 with lines" \
+    "pause 2" \
+    "reread"
 
 echo "$DISPLAY"
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 echo "$timestamp 100" >> "$TEMP_DATA_FILE"
-gnuplot "$TEMP_GNUPLOT_CONFIG_FILE" &
+gnuplot -p "$TEMP_GNUPLOT_CONFIG_FILE" &
 
-while true; do
+fetch_and_process_data() {
+    local data
     data=$(curl -s -X GET -u "${USER}:${PASSWORD}" -H "Content-Type:application/json" "${NEURIO_IP}/current-sample" | jq '.channels[] | select(.type == "CONSUMPTION").p_W')
-    if [ $? -eq 0 ]; then
-      if [[ -z "$data" ]]; then
-            continue
-        fi
+    if [ "$data" -ne 0 ]; then
         timestamp=$(date +"%Y-%m-%d %H:%M:%S")
         echo "$timestamp $data" >> "$TEMP_DATA_FILE"
         sleep 2
     else
         logger "something went wrong with neurio datapoint $timestamp"
     fi
+}
+
+while true; do
+    fetch_and_process_data
 done
